64ec9212a117b23353452f185ba43cac751498d6
Tyjoo26 <Tyjoo21@gmail.com>
2017-07-24 17:24:05 -0600

    Finish Count Method

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -8,8 +8,8 @@ require 'pry'
 class DeckTest < MiniTest::Test
 
 def test_does_it_exist?
-  deck = Deck.new
-
+  deck = Deck.new("")
+#why does it need an argument specifically an empty string?
   assert_instance_of Deck, deck
 end
 
@@ -21,6 +21,15 @@ def test_cards
 
 
   assert_equal [card_1, card_2, card_3], deck.cards
+end
+
+def test_cards_count
+  card_1 = Card.new("What is the capital of Alaska?", "Juneau")
+  card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars")
+  card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west")
+  deck = Deck.new([card_1, card_2, card_3])
+
+  assert_equal 3, deck.count
 end
 
 
